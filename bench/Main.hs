5e1221ce7f567482f0686e2af85059e0a540ecb1
Koz Ross <koz.ross@retro-freedom.nz>
2021-04-01 13:18:35 +1300

    Use bcompare

--- bench/Main.hs
+++ bench/Main.hs
@@ -7,7 +7,7 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.IO (Handle, IOMode (ReadMode), withFile)
-import Test.Tasty.Bench (bench, bgroup, defaultMain, nf)
+import Test.Tasty.Bench (bcompare, bench, bgroup, defaultMain, nf)
 import Text.Ascii (AsciiText)
 import qualified Text.Ascii as TA
 
@@ -29,24 +29,24 @@ runSherlockTests asText asAsciiText =
     [ bgroup
         "Matching"
         [ bench "Match, Text" . nf (T.count "Sherlock") $ asText,
-          bench "Match, AsciiText" . nf (TA.count [TA.ascii| "Sherlock" |]) $ asAsciiText,
+          bcompare "$NF == \"Match, Text\"" . bench "Match, AsciiText" . nf (TA.count [TA.ascii| "Sherlock" |]) $ asAsciiText,
           bench "Near-miss, Text" . nf (T.count "Sebastian") $ asText,
-          bench "Near-miss, AsciiText" . nf (TA.count [TA.ascii| "Sebastian" |]) $ asAsciiText,
+          bcompare "$NF == \"Near-miss, Text\"" . bench "Near-miss, AsciiText" . nf (TA.count [TA.ascii| "Sebastian" |]) $ asAsciiText,
           bench "Match 1, Text" . nf (T.count "S") $ asText,
-          bench "Match 1, AsciiText" . nf (TA.count [TA.ascii| "S" |]) $ asAsciiText,
+          bcompare "$NF == \"Match 1, Text\"" . bench "Match 1, AsciiText" . nf (TA.count [TA.ascii| "S" |]) $ asAsciiText,
           bench "No match, Text" . nf (T.count "Simping") $ asText,
-          bench "No match, AsciiText" . nf (TA.count [TA.ascii| "Simping" |]) $ asAsciiText
+          bcompare "$NF == \"No match, Text\"" . bench "No match, AsciiText" . nf (TA.count [TA.ascii| "Simping" |]) $ asAsciiText
         ],
       bgroup
         "Replacement"
         [ bench "Replace, Text" . nf (T.replace "Sherlock" "Moriarty") $ asText,
-          bench "Replace, AsciiText" . nf (TA.replace [TA.ascii| "Sherlock" |] [TA.ascii| "Moriarty" |]) $ asAsciiText,
+          bcompare "$NF == \"Replace, Text\"" . bench "Replace, AsciiText" . nf (TA.replace [TA.ascii| "Sherlock" |] [TA.ascii| "Moriarty" |]) $ asAsciiText,
           bench "Replace near-miss, Text" . nf (T.replace "Sebastian" "Moriarty") $ asText,
-          bench "Replace near-miss, AsciiText" . nf (TA.replace [TA.ascii| "Sebastian" |] [TA.ascii| "Moriarty" |]) $ asAsciiText,
+          bcompare "$NF == \"Replace near-miss, Text\"" . bench "Replace near-miss, AsciiText" . nf (TA.replace [TA.ascii| "Sebastian" |] [TA.ascii| "Moriarty" |]) $ asAsciiText,
           bench "Replace 1, Text" . nf (T.replace "S" "M") $ asText,
-          bench "Replace 1, AsciiText" . nf (TA.replace [TA.ascii| "S" |] [TA.ascii| "M" |]) $ asAsciiText,
+          bcompare "$NF == \"Replace 1, Text\"" . bench "Replace 1, AsciiText" . nf (TA.replace [TA.ascii| "S" |] [TA.ascii| "M" |]) $ asAsciiText,
           bench "Replace missing, Text" . nf (T.replace "Simping" "Dancing") $ asText,
-          bench "Replace missing, AsciiText" . nf (TA.replace [TA.ascii| "Simping" |] [TA.ascii| "Dancing" |]) $ asAsciiText
+          bcompare "$NF == \"Replace missing, Text\"" . bench "Replace missing, AsciiText" . nf (TA.replace [TA.ascii| "Simping" |] [TA.ascii| "Dancing" |]) $ asAsciiText
         ],
       bgroup
         "Breaking"
